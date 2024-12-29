988eb825636f85e02091cfdc3637243128521062
ffaridhatul <faridhatulzikri11@gmail.com>
2024-12-29 19:29:43 +0700

    commit pertama Belajar JSON

---
+++ src/test/java/enigma_camp/json/BelajarJavaJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package enigma_camp.json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
